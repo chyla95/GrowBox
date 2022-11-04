af830d4e98ad22ddd799f569f7c9fbcd04c17a8b
chyla95 <chyla95@gmail.com>
2022-11-04 19:46:35 +0100

    Fix var naming

--- Plant.py
+++ Plant.py
@@ -5,10 +5,10 @@ from Devices.Switchables.WaterPump import WaterPump
 
 class Plant:
     
-    def __init__(self, name:str, soilMoistureThresholds:tuple[int, int], wateringDuration:int, soilMoistureSensor:SoilMoistureSensor, waterPump:WaterPump, clock: Clock):
+    def __init__(self, name:str, soilMoistureThresholds:tuple[int, int], wateringDuration_InSec:int, soilMoistureSensor:SoilMoistureSensor, waterPump:WaterPump, clock: Clock):
         self._name = name
         self._soilMoistureThresholds = soilMoistureThresholds       
-        self._wateringDuration = wateringDuration  
+        self._wateringDuration_InSec = wateringDuration_InSec  
         self._soilMoistureSensor = soilMoistureSensor
         self._waterPump = waterPump
         self._clock = clock
@@ -16,7 +16,7 @@ class Plant:
         self._pumpingTimer = Timer()
         self._lastWateringTime:int = 0
         self._wateringsCount:int = 0
-        self._minDelayBetweenWaterings:int = 0
+        self._minDelayBetweenWaterings_InSec:int = 0
 
     # Properties
     @property
@@ -36,20 +36,20 @@ class Plant:
         return self._wateringsCount
 
     @property
-    def MinDelayBetweenWaterings(self):
-        return self._minDelayBetweenWaterings
-    @MinDelayBetweenWaterings.setter
-    def MinDelayBetweenWaterings(self, value):
-        if(value < self.WateringDuration):
-            print(f'Value {value} is too small adn cannot be set as a "MinDelayBetweenWaterings". The value should be >= {self.WateringDuration}')
-        self._minDelayBetweenWaterings = value
+    def MinDelayBetweenWaterings_InSec(self):
+        return self._minDelayBetweenWaterings_InSec
+    @MinDelayBetweenWaterings_InSec.setter
+    def MinDelayBetweenWaterings_InSec(self, value):
+        if(value < self.WateringDuration_InSec):
+            print(f'Value {value} is too small adn cannot be set as a "MinDelayBetweenWaterings". The value should be >= {self.WateringDuration_InSec}')
+        self._minDelayBetweenWaterings_InSec = value
         
     @property
-    def WateringDuration(self):
-        return self._wateringDuration
-    @WateringDuration.setter
-    def WateringDuration(self, value):
-        self._wateringDuration = value
+    def WateringDuration_InSec(self):
+        return self._wateringDuration_InSec
+    @WateringDuration_InSec.setter
+    def WateringDuration_InSec(self, value):
+        self._wateringDuration_InSec = value
 
     # Methods
     def GetSoilMoistureInPercent(self) -> int:
@@ -57,7 +57,7 @@ class Plant:
     
     def Water(self) -> None:
         try:
-            self._waterPump.TurnOnTemporarily(self.WateringDuration)
+            self._waterPump.TurnOnTemporarily(self.WateringDuration_InSec)
             self._wateringsCount += 1
             self._lastWateringTime = self._clock.TimeStamp
         except RuntimeError as err:
@@ -75,8 +75,8 @@ class Plant:
         if(not self.NeedsWatering()):
             # print(f'{self.Name} doesnt need watering now!')
             return False
-        if(not (Clock.TimeDiffInSeconds(self._clock.TimeStamp, self. LastWateringTime) >= self._minDelayBetweenWaterings)):
-            # print(f'{self.Name} was watered not that long ago!')
+        if(not (Clock.TimeDiffInSeconds(self._clock.TimeStamp, self. LastWateringTime) >= self._minDelayBetweenWaterings_InSec)):
+            # print(f'{self.Name} was watered recently!')
             return False        
         return True
             
